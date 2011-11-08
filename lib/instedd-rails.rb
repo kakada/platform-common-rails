798aedd21013faea59a06bf812a8428654fdd249
nekron <marianoabelcoca@gmail.com>
2011-11-08 15:18:37 -0300

    generator working from the rakefile

--- lib/instedd-rails.rb
+++ lib/instedd-rails.rb
@@ -5,7 +5,6 @@ require "devise_helper"
 require "mailer_helper"
 require "instedd_rails_engine"
 require "instedd-rails/config"
-require "generators/instedd-rails/config_generator"
 
 module Instedd
   module Rails
