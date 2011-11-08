36bc023b18dcd415201c57c863db31d19edef2bd
nekron <marianoabelcoca@gmail.com>
2011-11-08 12:57:38 -0300

    Views and layout added

---
+++ lib/devise_helper.rb
@@ -0,0 +1,13 @@
+module DeviseHelper  
+  def devise_error_messages!(html_options = {})
+    return if resource.errors.full_messages.empty?
+    
+    (content_tag :div, :class => "box error_description #{html_options[:class] || 'w60'}"  do
+      (content_tag :h2, 'The following errors occurred') \
+      + \
+      (content_tag :ul do
+        raw resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+      end)
+    end)
+  end
+end
---
+++ lib/instedd_rails_engine.rb
@@ -0,0 +1,6 @@
+module Instedd
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
