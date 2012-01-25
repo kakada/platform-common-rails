d4d2031bd818e56599db4fe3822a359fa59474a6
Ary Borenszweig <ary@esperanto.org.ar>
2012-01-25 15:05:37 -0300

    Fixed for Rails 3.2

--- lib/instedd_rails/engine.rb
+++ lib/instedd_rails/engine.rb
@@ -5,9 +5,7 @@ require File.expand_path("../../../app/helpers/devise_helper", __FILE__)
 
 module InsteddRails
   class Engine < ::Rails::Engine
-    
     initializer "change view paths order to prepend devise views" do |app|
-      
       application_view_path = ActionController::Base.view_paths.to_a.first
       instedd_rails_view_path = ActionController::Base.view_paths.detect do |path|
         path.to_s.include? "instedd-rails"
@@ -15,13 +13,13 @@ module InsteddRails
 
       ActionController::Base.prepend_view_path instedd_rails_view_path
       ActionController::Base.prepend_view_path application_view_path
-      ActionController::Base.view_paths= ActionController::Base.view_paths.uniq.freeze
+      ActionController::Base.view_paths = ActionController::Base.view_paths.uniq.freeze unless Rails::version >= '3.2'
     end
 
-    config.to_prepare do 
-      ApplicationController.helper(InsteddRails::ApplicationHelper)
-      ApplicationController.helper(InsteddRails::InsteddAppHelper)
-      ApplicationController.helper(InsteddRails::MailerHelper)
+    config.to_prepare do
+      ApplicationController.helper InsteddRails::ApplicationHelper
+      ApplicationController.helper InsteddRails::InsteddAppHelper
+      ApplicationController.helper InsteddRails::MailerHelper
     end
   end
 end
